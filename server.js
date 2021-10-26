1a9d99e157c4de27d2bd6c53e5a2e1b4fee09522
Lucy Liu <lucy.liu94@gmail.com>
2021-10-26 09:06:41 -0400

    day 6 superfruits

--- server.js
+++ server.js
@@ -1,239 +1,57 @@
-//////////////
-//IMPORT OUR DEPENDENCIES
-/////////////
-
-require("dotenv").config() //bring in .env vars
+///////////////////////////////////
+// Import our Dependencies
+///////////////////////////////////
+require("dotenv").config() // brings in .env vars
 const express = require("express") // web framework
-const morgan = require("morgan") //logger 
+const morgan = require("morgan") // logger
 const methodOverride = require("method-override") // to swap request methods
-const mongoose = require("mongoose") // our database library
-const path = require("path") //allow us to work with file paths //helped functions for file paths 
+const path = require("path") // helper functions for file paths
+const FruitsRouter = require("./controllers/fruit")
 
-///////////////
-//Establish Database Connection
-///////////////
-//setup the inputs from mongoose connect
-const DATABASE_URL = process.env.DATABASE_URL // url from .env
-const CONFIG = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}
 
-//Connect to Mongo
-mongoose.connect(DATABASE_URL, CONFIG)
-
-// our connection messages
-mongoose.connection
-.on("open", () => console.log("Connected to Mongoose"))
-.on("close", () => console.log("Disconnected from Mongoose"))
-.on("error", (error) => console.log(error))
-
-///////////////////
-// CREATE OUR FRUIT MODEL
-//////////////////
-const {Schema, model} = mongoose //destructuring Schema and model from mongoose 
-
-// const Schema = mongoose.Schema
-// const model = mongoose.Model
-
-// make a fruits schema
-const fruitSchema = new Schema({
-    name: String,
-    color: String,
-    readyToEat: Boolean,
-})
-
-//make the Fruit Model
-const Fruit = model ("Fruit", fruitSchema)
-
-//log the model to make sure it exists
-// console.log(Fruit)
-
-////////////
-//Create our app with object, configure liquid 
-///////////
-//import liquid
+/////////////////////////////////
+// Create our app with object, configure liquid
+/////////////////////////////////
+// import liquid
 const liquid = require("liquid-express-views")
-const { appendFile } = require("fs")
-//construct an absolute path to our views folder
+// construct an absolute path to our views folder
 const viewsFolder = path.resolve(__dirname, "views/")
 
-//log to see the value of viewsFolder
+// log to see the value of viewsFolder
 // console.log(viewsFolder)
 
-
-//create an app object with liquid, passing the path to the views folder 
+// create an app object with liquid, passing the path to the views folder
 const app = liquid(express(), {root: viewsFolder})
 
-//console.log app to confirm it exists  
+// console.log app to confirm it exists
 // console.log(app)
 
-//////////////////
-//Register our Middleware
-/////////////////
-//logging
+
+/////////////////////////////////////////////
+// Register Our Middleware
+/////////////////////////////////////////////
+// logging
 app.use(morgan("tiny"))
-//ability to override request methods
+// ability to override request methods
 app.use(methodOverride("_method"))
-//ability to parse urlencoded from form submission
-app.use(express.urlencoded({extended:true}))
-// setup our public folder to serve files statically 
+// ability to parse urlencoded from for submission
+app.use(express.urlencoded({extended: true}))
+// setup our public folder to serve files statically
 app.use(express.static("public"))
 
+////////////////////////////////////////
+// Routes
+/////////////////////////////////////////
 
-////////////////
-//Routes
-///////////////
-
-app.get("/", (req,res) =>{
+app.get("/", (req, res) => {
     res.send("your server is running... better catch it")
 })
 
-////////////////
-//fruit routes
-///////////////
+//Register Fruits Router
+app.use("/fruits", FruitsRouter)
 
-// seed route - seed our starter data
-
-app.get("/fruits/seed", (req,res) => {
-    //array of starter fruits
-    const startFruits = [
-        { name: "Orange", color: "orange", readyToEat: false },
-        { name: "Grape", color: "purple", readyToEat: false },
-        { name: "Banana", color: "orange", readyToEat: false },
-        { name: "Strawberry", color: "red", readyToEat: false },
-        { name: "Coconut", color: "brown", readyToEat: false },
-      ];
-
-      //delete all fruits
-      Fruit.deleteMany({})
-      .then((data) => {})
-        //seed the starter fruits
-        Fruit.create(startFruits)
-        .then((data) =>{
-            //send created fruits back as JSON
-            res.json(data)
-        })
-})
-
-//index route - get - /fruits
-app.get("/fruits", (req,res) =>{
-    //find all the fruits 
-    Fruit.find({}) //passing na empty objects find ALL
-    .then((fruits) => {
-        //render the index template with the fruits
-        res.render("fruits/index.liquid",{fruits})
-    })
-
-    //error handling 
-    .catch((error) => {
-        res.json({error})
-    })
-
-})
-
-//new route - get - "/fruits/new"
-
-app.get("/fruits/new", (req,res) => {
-    res.render("fruits/new.liquid")
-})
-
-
-// create - post request -/fruits
-app.post("/fruits", (req,res) => {
-    // convert the checkbox property to true or false
-    req.body.readyToEat = req.body.readyToEat === "on" ? true : false //using ternary operator 
-
-    //create the new fruit
-    Fruit.create(req.body)
-    .then((fruit) => {
-        //redirect the user back to the index route
-        res.redirect("/fruits")
-    })
-
-     //error handling 
-     .catch((error) => {
-        res.json({error})
-    })
-
-})
-
-// update route - put request - "/fruits/:id"
-app.put("/fruits/:id", (req, res) => {
-    // get the id from params
-    const id = req.params.id
-    
-    // convert the checkbox property to true or false
-    req.body.readyToEat = req.body.readyToEat === "on" ? true : false
-
-    // update the item with the matching id
-    Fruit.findByIdAndUpdate(id, req.body, {new: true})
-    .then((fruit) => {
-        // redirect user back to index
-        res.redirect("/fruits")
-    })
-     // error handling
-     .catch((error) => {
-        res.json({error})
-    })
-}
-)
-
-// edit route - get request - /fruits/:id/edit
-app.get("/fruits/:id/edit", (req, res) => {
-    // get the id from params
-    const id = req.params.id
-
-    // get the fruit with the matching id
-    Fruit.findById(id)
-    .then((fruit) => {
-        // render the edit page template with the fruit data
-        res.render("fruits/edit.liquid", { fruit })
-    })
-    // error handling
-    .catch((error) => {
-        res.json({error})
-    })
-})
-
-
-// destroy route - delete request - /fruits/:id
-app.delete("/fruits/:id", (req,res) =>{
-    // grab the idea from params
-    const id = req.params.id
-    // delete the fruit
-    Fruit.findByIdAndRemove(id)
-    .then((fruit) =>{})
-        res.redirect("/fruits")
-
-        // error handling
-    .catch((error) => {
-        res.json({error})
-    })
-})
-
-// show route - get - /fruits/:id
-app.get("/fruits/:id", (req, res) => {
-    // get the id from params
-    const id = req.params.id
-
-    // get that particular fruit from the database
-    Fruit.findById(id)
-    .then((fruit) => {
-        // render the show template with the fruit
-        res.render("fruits/show.liquid", {fruit})
-    })
-    // error handling
-    .catch((error) => {
-        res.json({error})
-    })
-})
-
-
-
-//////////
-//Setup Server Listener
-//////////
+/////////////////////////////////////////////
+// Setup Server Listener
+/////////////////////////////////////////////
 const PORT = process.env.PORT // grabbing the port number from env
 app.listen(PORT, () =>  console.log(`Listening on port ${PORT}`))
-
